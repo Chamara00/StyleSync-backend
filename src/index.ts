1b012f9b7c5670a266843dc0d0f2e991cf1e345e
Chamara00 <chamara.wimalaweera@gmail.com>
2024-02-11 19:46:02 +0530

    Created token

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,34 @@ import express from 'express';
 // import { Request, Response } from 'express';
 //import { configureRoutes } from './routes';
 import { registerSalonStep1 } from './controllers/mobile-salon-register';
-import { registerStaff } from './controllers/mobile-staff-register';
-
+import { registerStaff } from './controllers/RegisterStaff/mobile-staff-register';
+import {createOpenHours} from './controllers/OpenDaysAndHours/mobile-salon-create-open-hours';
+import { getOpendaysAndHours } from './controllers/OpenDaysAndHours/mobile-salon-opendays-and-hours';
+import { updateOpenHours } from './controllers/OpenDaysAndHours/mobile-salon-update-open-and-close-hours';
+import { deleteBreaks } from './controllers/Breaks/mobile-delete-breaks'; 
+import { createBreak } from './controllers/Breaks/mobile-create-salon-breaks'; 
+import { getBreaks } from './controllers/Breaks/mobile-view-salon-breaks';
+import { updateBreaks } from './controllers/Breaks/mobile-salon-update-breaks';
+// import { createNewService } from './controllers/Services/admin-store-services';
+// import  { createStaffService } from './controllers/Services/mobile-staff-service-create';
 const app = express();
 
 app.use(express.json());
 
 app.post('/register-salon/step1', registerSalonStep1);
 app.post('/register-staff', registerStaff);
+app.post('/update-open-hours', createOpenHours);
+app.post('/create-break',createBreak);
+// app.post('/create-new-service', createNewService);
+// app.post('/create-staff-service', createStaffService);
 
+app.get('/get-opendays-and-hours', getOpendaysAndHours);
+app.get('/get-breaks',getBreaks);
+
+app.put('/update-open-close-hours', updateOpenHours);
+app.put('/update-breaks',updateBreaks);
+
+app.delete('/delete-break', deleteBreaks);
 
 const PORT = process.env.PORT || 8000;
 
