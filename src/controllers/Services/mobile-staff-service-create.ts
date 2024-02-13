12a36f491ea11f4fe52bc97c51ea7f8a142276de
Anuradha00Herath <herathanuradha03@gmail.com>
2024-02-13 22:21:33 +0530

    modify staff service modules

--- src/controllers/Services/mobile-staff-service-create.ts
+++ src/controllers/Services/mobile-staff-service-create.ts
@@ -9,7 +9,7 @@ export async function createStaffService(req: Request, res: Response) {
         if (!staffId || !serviceType) {
             return res.status(400).json({ status: 400, error: 'Inputs not found' });
         } else {
-            const selectedServices = [];
+            // const selectedServices = [];
             for (let i = 0; i < serviceType.length; i++) {
                 const services = await prisma.allServices.findMany({
                     where: {
@@ -19,10 +19,8 @@ export async function createStaffService(req: Request, res: Response) {
                         service: true,
                     }
                 });
-                selectedServices.push(...services);
+                // selectedServices.push(...services);
                 const existingServiceName = services.map(service => service.service);
-                // const existingServicePrice = services.map(service => service.price);
-                // const existingServiceDuration = services.map(service => service.duration);
                 for(let j =0; j < existingServiceName.length; j++ ){
                     const getStaffServices = await prisma.allServices.findMany({
                         where:{
@@ -37,7 +35,7 @@ export async function createStaffService(req: Request, res: Response) {
                     const existingServiceType = getStaffServices.map(service => service.serviceType);
                     const existingPrice = getStaffServices.map(service => service.price);
                     const existingDuration = getStaffServices.map(service => service.duration);
-                    const storeServices = await prisma.service.create({
+                    prisma.service.create({
                         data: {
                             name:existingServiceName[j],
                             serviceType: existingServiceType[0],
@@ -54,16 +52,15 @@ export async function createStaffService(req: Request, res: Response) {
                         }
                     });
                     const existingId = getServices.map(service => service.id);
-                    const storeStaffServices = await prisma.serviceStaff.create({
+                    await prisma.serviceStaff.create({
                         data: {
                             serviceId: existingId[0],
                             staffId
                         }
                     });
                 }
-                 res.status(200).json({ status: 200, message: 'service created' });
             }
-            return res.status(200).json({ status: 200, data: selectedServices });
+            return res.status(200).json({ status: 200, message: 'service created' });
         }
     } catch (error) {
         console.log(error);
