41522f9f83b1266dc1116cf0026a99c2c70878c9
Chamara00 <chamara.wimalaweera@gmail.com>
2024-03-11 19:57:35 +0530

    Created email verification

--- src/controllers/RegisterSalon/mobile-salon-register.ts
+++ src/controllers/RegisterSalon/mobile-salon-register.ts
@@ -2,7 +2,7 @@
 
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
-import jwt from 'jsonwebtoken';
+import { generateToken } from '../../utils/jwtUtils';
 import nodemailer from 'nodemailer';
 
 const prisma = new PrismaClient();
@@ -17,7 +17,7 @@ export async function registerSalonStep1(req: Request, res: Response){
         }
 
         const existingSalon = await prisma.salon.findUnique({
-            where: {contactNo}
+            where: {contactNo, email}
         });
 
         if(existingSalon){
@@ -36,14 +36,17 @@ export async function registerSalonStep1(req: Request, res: Response){
             },
         });
 
-        const token = jwt.sign({ userId: newSalonStep1.id}, process.env.JWT_SECRET ||'fallback-secret', { expiresIn: '24h' } );
+        const token = generateToken({ id: newSalonStep1.id, email: newSalonStep1.email });
 
         return res.status(201).json({ status: 201, message: 'Step 1 successful', token});
     }
-    catch(error){
-        console.log(error);
-        return res.status(500).json({ status: 500, error: 'Failed to prcoess step 1'});
-    }
+    catch (error: unknown) {
+        if (error instanceof Error) {
+          res.status(400).json({ error: error.message });
+        } else {
+          res.status(500).json({ error: 'Internal Server Error' });
+        }
+      }
     finally{
         await prisma.$disconnect();
     }
@@ -59,7 +62,7 @@ async function sendOTP(email: string, otp: string){
         service: 'gmail',
         auth: {
             user: 'stylesync26@gmail.com',
-            pass: 'Style@#12Sync'
+            pass: 'ngtbfviurhciiqjp'
         }
     });
 
