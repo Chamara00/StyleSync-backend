c778ba06e435f8af0149f700afbcb46b6de01e1c
Anuradha00Herath <herathanuradha03@gmail.com>
2024-04-11 12:04:39 +0530

    Create function to get staff id (7)

--- src/controllers/CustomerWeb/customer-register.ts
+++ src/controllers/CustomerWeb/customer-register.ts
@@ -1,42 +1,42 @@
-//Customer Register Using Web
+// //Customer Register Using Web
 
-import { Request, Response } from 'express';
-import { PrismaClient} from '@prisma/client';
+// import { Request, Response } from 'express';
+// import { PrismaClient} from '@prisma/client';
 
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export async function registerCustomer(req: Request, res: Response){
-    const { email, password, confirmPassword} = req.body;
+// export async function registerCustomer(req: Request, res: Response){
+//     const { email, password, confirmPassword} = req.body;
 
-    try{
-        if (!email || !password || !confirmPassword){
-            return res.status(400).json({ status: 400, error:'Invlid input format'});
-        }
-        if (password !== confirmPassword){
-            return res.status(400).json({ status: 400, error:'Password are not match'});
-        }
-        const userExist = await prisma.customer.findUnique({
-            where: {email}
-        });
-        if (userExist){
-            return res.status(400).json({ status: 400, error: 'User with this email already exisits'});
-        }
+//     try{
+//         if (!email || !password || !confirmPassword){
+//             return res.status(400).json({ status: 400, error:'Invlid input format'});
+//         }
+//         if (password !== confirmPassword){
+//             return res.status(400).json({ status: 400, error:'Password are not match'});
+//         }
+//         const userExist = await prisma.customer.findUnique({
+//             where: {email}
+//         });
+//         if (userExist){
+//             return res.status(400).json({ status: 400, error: 'User with this email already exisits'});
+//         }
         
-            await prisma.customer.create({
-            data : {
-                email,
-                password
-            },
-        });
+//             await prisma.customer.create({
+//             data : {
+//                 email,
+//                 password
+//             },
+//         });
 
-        return res.status(201).json({ status: 201, message: 'Customer Registraion successful'});
-    }
-    catch(error){
-        console.log(error);
-        return res.status(500).json({ status: 500, error: 'Failed to prcoess Registration'});
-    }
-    finally{
-        await prisma.$disconnect();
-    }
-}
+//         return res.status(201).json({ status: 201, message: 'Customer Registraion successful'});
+//     }
+//     catch(error){
+//         console.log(error);
+//         return res.status(500).json({ status: 500, error: 'Failed to prcoess Registration'});
+//     }
+//     finally{
+//         await prisma.$disconnect();
+//     }
+// }
