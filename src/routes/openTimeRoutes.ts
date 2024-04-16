d72006b593db4b7acb179af23200dee5c6c0ca86
Anuradha00Herath <herathanuradha03@gmail.com>
2024-04-16 22:28:39 +0530

    change open-time-rotes

--- src/routes/openTimeRoutes.ts
+++ src/routes/openTimeRoutes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
-// import { createOpenHours } from '../controllers/OpenDaysAndHours/Create/mobile-salon-create-open-hours';
-// import { getOpendaysAndHours } from '../controllers/OpenDaysAndHours/View/mobile-salon-opendays-and-hours';
-// import { updateOpenHours } from '../controllers/OpenDaysAndHours/Update/mobile-salon-update-open-and-close-hours';
+import { getOpendaysAndHours } from '../controllers/OpenDaysAndHours/View/mobile-salon-opendays-and-hours';
+import { updateOpenHours } from '../controllers/OpenDaysAndHours/Update/mobile-salon-update-open-and-close-hours';
 import { deleteBreaks } from '../controllers/Breaks/Delete/mobile-delete-breaks'; 
 import { createBreak } from '../controllers/Breaks/Create/mobile-create-salon-breaks'; 
 import { getBreaks } from '../controllers/Breaks/View/mobile-view-salon-breaks';
@@ -14,10 +13,10 @@ export function configureopenTimeRoutes(router: Router): void {
 
    router.post('/create-break',createBreak);
 
-    // router.get('/get-opendays-and-hours', getOpendaysAndHours);
+    router.get('/get-opendays-and-hours', getOpendaysAndHours);
     router.get('/get-breaks',getBreaks);
 
-    // router.put('/update-open-close-hours', updateOpenHours);
+    router.put('/update-open-close-hours', updateOpenHours);
     router.put('/update-breaks',updateBreaks);
 
     router.delete('/delete-break', deleteBreaks);
