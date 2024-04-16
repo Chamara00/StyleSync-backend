621a369871086b1e65dc79509cdd2c8c00bfcbbd
Anuradha00Herath <herathanuradha03@gmail.com>
2024-04-16 22:18:09 +0530

    change mobile-create-salon-breaks

--- src/controllers/Breaks/Create/mobile-create-salon-breaks.ts
+++ src/controllers/Breaks/Create/mobile-create-salon-breaks.ts
@@ -5,33 +5,26 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function createBreak(req: Request, res: Response) {
+  const { staffId, dayName, breakStart, breakEnd } = req.body;
 
-
-    const { staffId, dayName } = req.body;
-
-    try{
-        if(!staffId || !dayName){
-
-            return res.status(400).json({ status: 400, error: 'inputs not found' });
-        }
-        else{
-            const createBreaks = await prisma.breaks.create({
-                data: {
-
-                    staffId,
-
-                    dayName,
-                    breakStart:'12:00',
-                    breakEnd:'13:00'
-                },
-            });
-            return res.status(201).json({ status: 201, message: 'Step 1 successful', breaks: createBreaks });
-        }
+  try {
+    if (!staffId || !dayName || breakStart || breakEnd) {
+      return res.status(400).json({ status: 400, error: 'inputs not found' });
+    } else {
+      const createBreaks = await prisma.breaks.create({
+        data: {
+          staffId,
+          dayName,
+          breakStart,
+          breakEnd
+        },
+      });
+      return res.status(201).json({ status: 201, message: 'Step 1 successful', breaks: createBreaks });
     }
-    catch (error) {
-        console.log(error);
-        return res.status(500).json({ status: 500, error: 'Failed to process step 1' });
-    } finally {
-        await prisma.$disconnect();
-    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ status: 500, error: 'Failed to process step 1' });
+  } finally {
+    await prisma.$disconnect();
+  }
 }
