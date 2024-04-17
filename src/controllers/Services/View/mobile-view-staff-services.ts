63acc5b601c571e62e38d13dd7cdb997b081d2af
Anuradha00Herath <herathanuradha03@gmail.com>
2024-04-18 01:13:11 +0530

    update mobile-view-staff-services

--- src/controllers/Services/View/mobile-view-staff-services.ts
+++ src/controllers/Services/View/mobile-view-staff-services.ts
@@ -4,30 +4,20 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function getServiceType(req: Request, res: Response) {
-    const { staffId, serviceType } = req.query;
+    const { staffId, serviceType } = req.body;
     try{
-        if(!staffId || !serviceType || typeof staffId !== 'string'){
+        if(!staffId || !serviceType){
             return res.status(400).json({ status: 400, error: 'salon id not found' });
         }
-        else{
-            const viewService = await prisma.service.findMany ({
-                where:{
-                 serviceType: String(serviceType),
-                },
-                select:{
-                 serviceStaff: {
-                     where: {
-                         staffId: parseInt(staffId)
-                     },
-                     select :{
-                         serviceId: true
-                     }
-                 }
-                }
-             });
-             //const existingServiceId = viewService.map(item =>item.serviceStaff);
-             return res.status(200).json({ status: 200, data: viewService });
-        }
+        const viewService = await prisma.service.findMany ({
+            where: {
+                serviceType:serviceType
+            },
+            select: {
+                name: true
+            }
+        });
+        return res.status(200).json({ status: 200, data: viewService });
     } catch (error) {
         console.log(error);
         return res.status(500).json({ status: 500, error: 'Failed to process' });
