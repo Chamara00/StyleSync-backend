cae00e7501cd77b30d675c414828215703f30725
Anuradha00Herath <herathanuradha03@gmail.com>
2024-04-18 02:01:28 +0530

    update mobile-view-staff-services(4)

--- src/controllers/Services/View/mobile-view-staff-service-info.ts
+++ src/controllers/Services/View/mobile-view-staff-service-info.ts
@@ -8,7 +8,7 @@ const prisma = new PrismaClient();
 export async function getServiceInfo(req: Request, res: Response) {
     const { staffId, serviceType } = req.query;
     try{
-        if(!staffId || !serviceType){
+        if(!staffId){
             return res.status(400).json({ status: 400, error: 'staff id not found' });
         }
         const viewService = await prisma.serviceStaff.findMany ({
