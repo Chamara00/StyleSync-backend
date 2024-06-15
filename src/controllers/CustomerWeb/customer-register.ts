57d50ba07f9c3108cffbc55e31fc21cbfc0eddc8
Anuradha00Herath <herathanuradha03@gmail.com>
2024-06-15 14:19:42 +0530

    create customer register again

--- src/controllers/CustomerWeb/customer-register.ts
+++ src/controllers/CustomerWeb/customer-register.ts
@@ -1,42 +1,30 @@
+import { Request,Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
-// import { Request, Response } from 'express';
-// import { PrismaClient } from '@prisma/client';
-// import bcrypt from 'bcrypt';
+const prisma = new PrismaClient();
 
-// const prisma = new PrismaClient();
-
-// export async function RegisterCustomer(req: Request, res: Response) {
-//   const { email, password, confirmPassword, name } = req.body; //request from body
-
-//   try {
-//     if (!email || !password || !confirmPassword || !name) {
-//       return res.status(400).json({ status: 400, error: 'Invalid input format' });
-//     }
-//     if (password !== confirmPassword) {
-//       return res.status(400).json({ status: 400, error: 'Passwords do not match' });
-//     }
-//     const userExist = await prisma.customer.findUnique({
-//       where: { email },
-//     });
-
-//     if (userExist) {
-//       return res.status(400).json({ status: 400, error: 'User with this email already exists' });
-//     } else {
-//       const hashedPassword = await bcrypt.hash(password, 10);
-//       const register = await prisma.customer.create({
-//         data: {
-//           name: String(name),
-//           email: String(email),
-//           password: String(hashedPassword),
-//         },
-//       });
-
-//       return res.status(201).json({ status: 201, message: 'Customer Registration successful', data: register });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).json({ status: 500, error: 'Failed to process Registration' });
-//   } finally {
-//     await prisma.$disconnect();
-//   }
-// }
+export async function CustomerRegister(req:Request,res:Response) {
+    const {name, email,password, confirmPassword} = req.body;
+    try{
+    if(!name || !email || !password || !confirmPassword){
+        return res.status(400).json({message:'input not found'});
+    }else if(password !== confirmPassword){
+        return res.status(400).json({message:'password not match'});
+    }
+    else{
+        const customer = await prisma.customer.create({
+            data:{
+                name,
+                email,
+                password
+            }
+        });
+        return res.status(200).json({status: 200, message: 'Register Customer Successfully', data:customer});
+    }
+    }catch (error) {
+        console.log(error);
+        return res.status(500).json({ status: 500, error: 'Failed to process' });
+    } finally {
+        await prisma.$disconnect();
+    }
+}
--- src/routes/customer/customer-home-routes.ts
+++ src/routes/customer/customer-home-routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { ShowAvailableCategories } from '../../controllers/CustomerWeb/Show-category';
+import { CustomerRegister } from '../../controllers/CustomerWeb/customer-register';
 
 export function ConfigureCustomerHomeRoutes(router: Router): void {
     router.get('/get-all-categories', ShowAvailableCategories);
+    router.post('/register-customer',CustomerRegister);
 }
