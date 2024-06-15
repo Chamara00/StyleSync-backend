f417d964d891670702cbecda8d6a948468e07655
Chamara00 <chamara.wimalaweera@gmail.com>
2024-06-15 12:30:03 +0530

    commented customer-register

--- src/routes/showCustomerToSalon.ts
+++ src/routes/showCustomerToSalon.ts
@@ -3,12 +3,11 @@ import { Home } from '../controllers/CustomerWeb/Web-Home';
 //import { RegisterCustomer } from '../controllers/CustomerWeb/customer-register';
 import { LoginCustomer } from '../controllers/CustomerWeb/Login';
 import { SalonAddManual } from '../controllers/CustomerWeb/Salon-ManualAdd';
-import {SearchResult } from '../controllers/CustomerWeb/Search-salon';
-import{ FirstPage } from '../controllers/CustomerWeb/FirstPage';
+import { SearchResult } from '../controllers/CustomerWeb/Search-salon';
+import { FirstPage } from '../controllers/CustomerWeb/FirstPage';
 import { ShowAvailableCategories } from '../controllers/CustomerWeb/Show-category';
-import { RegisterCustomer } from '../controllers/CustomerWeb/customer-register';
+//mport { RegisterCustomer } from '../controllers/CustomerWeb/customer-register';
 // import { AppointmentSchedule } from '../controllers/CustomerWeb/AppointmentSchedule';
-
 
 //http://localhost:8000/app/v1/customer/show-salon-to-customer
 //http://localhost:8000/app/v1/customer/create-customer
@@ -16,17 +15,15 @@ import { RegisterCustomer } from '../controllers/CustomerWeb/customer-register';
 //http://localhost:8000/app/v1/customer/salon-Manual-Add
 //http://localhost:8000/app/v1/customer/search-salon
 
-
-
 //https://stylesync-backend-test.onrender.com/app/v1/time/create-open-hours
 
 export function configureCustomerRoutes(router: Router): void {
-    router.post('/show-salon-to-customer', Home);
-    router.post('/create-customer', RegisterCustomer);
-    router.post('/customer-login',LoginCustomer);
-    router.post('/salon-Manual-Add',SalonAddManual);
-    router.get('/search-salon',SearchResult);
-    router.get('/Home-Page',FirstPage);
-    router.get('/get-all-categories',ShowAvailableCategories);
-    // router.post('Appointment-Schedule',AppointmentSchedule);
+  router.post('/show-salon-to-customer', Home);
+  //router.post('/create-customer', RegisterCustomer);
+  router.post('/customer-login', LoginCustomer);
+  router.post('/salon-Manual-Add', SalonAddManual);
+  router.get('/search-salon', SearchResult);
+  router.get('/Home-Page', FirstPage);
+  router.get('/get-all-categories', ShowAvailableCategories);
+  // router.post('Appointment-Schedule',AppointmentSchedule);
 }
