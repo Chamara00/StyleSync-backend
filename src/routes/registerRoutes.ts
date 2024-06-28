b2d95e9ebd5d6cc1d567cfb2d2cb3ce517d21418
Anuradha00Herath <herathanuradha03@gmail.com>
2024-06-28 14:17:13 +0530

    add image to salon

--- src/routes/registerRoutes.ts
+++ src/routes/registerRoutes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { RegisterSalon } from '../controllers/RegisterSalon/mobile-salon-register';
 import {Login} from '../controllers/RegisterSalon/mobile-salon-login';
 import { AddSalonImage } from '../controllers/RegisterSalon/mobile-salon-add-image';
-import multer from 'multer';
+import multer = require('multer');
 //import { authenticateAdminToken } from '../middlewares/authMiddleware';
 const upload = multer({ dest: '../assets' });
 
