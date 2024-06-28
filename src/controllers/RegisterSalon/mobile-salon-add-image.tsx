6657267c44ef5f3b4204b4be0fc568e9b72ec975
Anuradha00Herath <herathanuradha03@gmail.com>
2024-06-28 14:03:45 +0530

    add image to salon

--- src/controllers/RegisterSalon/mobile-salon-add-image.tsx
+++ src/controllers/RegisterSalon/mobile-salon-add-image.tsx
@@ -4,25 +4,23 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const AddSalonImage = async (req: Request, res: Response) => {
-  const {image,salonId} = req.body;
-  if(!image || !salonId ){
-    return res.status(400).json({message:'inputs not found'});
-  }
+  const { salonImage, salonId } = req.body;
 
   try {
-    const salons = await prisma.salon.updateMany({
-        where:{
-            id:salonId
-        },
-      data:{
-        image:image
-      }
+    // Update the salon record in the database with the image path
+    const updatedSalon = await prisma.salon.update({
+      where: { id: salonId },
+      data: { image: salonImage }, // Assuming 'image' is the field in your salon table to store the path
     });
 
-    return res.status(200).json({status:200, message:'Add image successfully', data:salons});
-  }catch (error) {
-    console.log(error);
-    return res.status(500).json({ status: 500, error: 'Failed to process' });
+    return res.status(200).json({
+      status: 200,
+      message: 'Image added successfully',
+      data: updatedSalon,
+    });
+  } catch (error) {
+    console.error('Error adding image:', error);
+    return res.status(500).json({ status: 500, error: 'Failed to add image' });
   } finally {
     await prisma.$disconnect();
   }
