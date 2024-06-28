4c721e83e4336275397ec7e7d1685e709e55e14c
Anuradha00Herath <herathanuradha03@gmail.com>
2024-06-28 12:04:57 +0530

    add image to salon

--- src/routes/registerRoutes.ts
+++ src/routes/registerRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { RegisterSalon } from '../controllers/RegisterSalon/mobile-salon-register';
 import {Login} from '../controllers/RegisterSalon/mobile-salon-login';
+import { AddSalonImage } from '../controllers/RegisterSalon/mobile-salon-add-image';
 //import { authenticateAdminToken } from '../middlewares/authMiddleware';
 
 export function configureRegisterRoutes(router: Router): void {
     router.post('/register-salon', RegisterSalon);
     router.get('/salon-login',Login);
+    router.put('/add-salon-image',AddSalonImage);
 }
 
