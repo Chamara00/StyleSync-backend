badf9be8b5c521378e73ffb1f4a55405f4f28468
Asangika <asangikanethmini@gmail.com>
2024-06-29 21:24:01 +0530

    update StaffAvailability1

--- prisma/migrations/20240628110401_image_path_add/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `image` on the `salon` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "salon" DROP COLUMN "image",
-ADD COLUMN     "imagePath" TEXT;
