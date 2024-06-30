299fd08668e24e35d466177f3030a8cdee384658
Asangika <asangikanethmini@gmail.com>
2024-06-30 22:58:24 +0530

    create service

--- src/routes/openServiceRoutes.ts
+++ src/routes/openServiceRoutes.ts
@@ -7,6 +7,7 @@ import { deleteStaffService } from '../controllers/Services/Delete/mobile-staff-
 import { updateStaffServiceInfo } from '../controllers/Services/Update/mobile-staff-service-update';
 import { deleteStaffServiceType } from '../controllers/Services/Delete/mobile-staff-service-type-delete';
 import { getServiceType } from '../controllers/Services/View/mobile-view-staff-services';
+import {createNewService} from '../controllers/Services/Create/mobile-create-new-services';
 
 export function configureopenServicesRoutes(router: Router): void{
     router.post('/staff-service-create',createStaffService);
@@ -17,4 +18,6 @@ export function configureopenServicesRoutes(router: Router): void{
     router.delete('/delete-staff-service', deleteStaffService);
     router.delete('/delete-staff-service-type', deleteStaffServiceType);
     router.put('/update-staff-service-info', updateStaffServiceInfo);
+    router.post('/staff-new-service-create',createNewService);
+
 }
