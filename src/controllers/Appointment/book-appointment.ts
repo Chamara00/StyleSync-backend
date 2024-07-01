835621a18397aa476db66848f1c84e4ba8f72da8
Anuradha00Herath <herathanuradha03@gmail.com>
2024-07-01 23:57:17 +0530

    add book appointment

--- src/controllers/Appointment/book-appointment.ts
+++ src/controllers/Appointment/book-appointment.ts
@@ -28,26 +28,8 @@ export async function  BookAppointment(req: Request, res: Response){
                 }
                 }
         });
-        const customerAppointment = await prisma.customerAppointmentBlock.create({
-            data:{
-                customerId:userId,
-                date:date,
-                startTime:startTime,
-                staffId:staffId,
-                isCancel:false,
-                isReject:false,
-            }
-        });
-        const serviceAppointment = await prisma.serviceAppointmentBlock.create({
-            data:{
-                serviceId:serviceId,
-                date:date,
-                startTime:startTime,
-                staffId:staffId
-            }
-        });
 
-        return res.status(200).json({status:200, message:'Successfull',data:{appointment,customerAppointment,serviceAppointment}});
+        return res.status(200).json({status:200, message:'Successfull',data:appointment});
     }catch (error) {
     console.log(error);
     return res.status(500).json({ status: 500, error: 'Failed to process' });
