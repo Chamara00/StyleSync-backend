6e5e791ba1df17126cf83436c3b79428c0014654
Asangika <asangikanethmini@gmail.com>
2024-07-01 21:47:35 +0530

    notification

---
+++ src/routes/Notification.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+import { NewNotification} from '../controllers/Notification/mobile-new-notification';
+export function configureNotification(router: Router): void{
+    router.get('/get_new_appoinment',NewNotification);
+}
--- src/routes/app/v1/v1.ts
+++ src/routes/app/v1/v1.ts
@@ -6,6 +6,7 @@ import { configureopenServicesRoutes } from '../../openServiceRoutes';
 import { configurehealthRoutes } from '../../healthRoute';
 import { configuregetAppointmentToSalon } from '../../getAppointmentsToSalon';
 import{configuregetSalonDtails} from '../../SalonProfile';
+import {configureNotification} from '../../Notification';
 
 
 export function configureV1Routes(router: Router): void {
@@ -36,5 +37,9 @@ export function configureV1Routes(router: Router): void {
     const salonProfileRoutes = Router();
     configuregetSalonDtails(salonProfileRoutes);
     router.use('/SalonProfile',salonProfileRoutes);
+
+    const Notification = Router();
+    configureNotification(Notification);
+    router.use('/notification',Notification );
 
 }
