35f3110e99ca11b3b2df1700f6c641a98004906a
Anuradha00Herath <herathanuradha03@gmail.com>
2024-07-03 04:45:28 +0530

    add appointment details

---
+++ src/controllers/Appointment/get-appointment-details.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+//3.1 view breaks
+
+const prisma = new PrismaClient();
+
+export async function getAppointments(req: Request, res: Response) {
+
+    const { userId } = req.query;
+    try{
+        if(!userId){
+
+            return res.status(400).json({ status: 400, error: 'salon id not found' });
+        }
+        const appointments = await prisma.customerAppointmentBlock.findMany ({
+            where : {
+                customerId:Number(userId)
+            },
+            select: {
+                date:true,
+                startTime:true,
+                appointmentBlock:{
+                    select:{
+                        staff:{
+                            select:{
+                                salonStaff:{
+                                    select:{
+                                        salon:{
+                                            select:{
+                                                name:true
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        const existingAppointments = appointments.map((a)=>({
+            date:a.date,
+            startTime:a.startTime,
+            salonName:a.appointmentBlock.staff.salonStaff.map((b)=>b.salon.name)
+        }));
+        return res.status(200).json({ status: 200, data: existingAppointments });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ status: 500, error: 'Failed to process' });
+    } finally {
+        await prisma.$disconnect();
+    }
+}
