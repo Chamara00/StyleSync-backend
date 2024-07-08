d5f0f14cfbbfae6f0df72320dc75467e7f9ca2bb
Asangika <asangikanethmini@gmail.com>
2024-07-08 13:15:51 +0530

    update salon otp

---
+++ src/controllers/RegisterSalon/email-verified.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function EmailVerified(req: Request, res: Response) {
+  const { salonId, otp } = req.body;
+  try {
+    if (!salonId || !otp) {
+      return res.status(400).json({ message: 'Please provide all the details' });
+    }
+    const getOtp = await prisma.salon.findMany({
+      where: {
+        id: salonId,
+      },
+      select: {
+        otp: true,
+      },
+    });
+
+    if(!getOtp[0].otp){
+        return res.status(400).json({ message: 'OTP not found' });
+    }
+    else if(getOtp[0].otp !== otp){
+        return res.status(400).json({ message: 'OTP not matched' });
+    }
+    else{
+        const verified = await prisma.salon.update({
+            where:{
+                id:salonId
+            },
+            data:{
+                isVerified: true
+            }
+        });
+        return res.status(200).json({status:200, message:'Success', data: verified});
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ status: 500, error: 'Failed to process' });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
--- src/routes/registerRoutes.ts
+++ src/routes/registerRoutes.ts
@@ -2,10 +2,14 @@ import { Router } from 'express';
 import { RegisterSalon } from '../controllers/RegisterSalon/mobile-salon-register';
 import { Login } from '../controllers/RegisterSalon/mobile-salon-login';
 import { AddSalonImage } from '../controllers/RegisterSalon/mobile-add-image';
+import {GenerateOTP} from '../controllers/RegisterSalon/generate-Otp';
+import {EmailVerified} from '../controllers/RegisterSalon/email-verified';
 //import { authenticateAdminToken } from '../middlewares/authMiddleware';
 
 export function configureRegisterRoutes(router: Router): void {
   router.post('/register-salon', RegisterSalon);
   router.get('/salon-login', Login);
   router.put('/update-salon-image', AddSalonImage);
+  router.put('/generate-salon-otp',GenerateOTP);
+  router.put('/verified-salon-email',EmailVerified);
 }
