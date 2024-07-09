d277691a3d73803e05e923b454940f65e0daa2f6
Asangika <asangikanethmini@gmail.com>
2024-07-09 12:00:35 +0530

    changes password

--- src/routes/registerRoutes.ts
+++ src/routes/registerRoutes.ts
@@ -4,6 +4,8 @@ import { Login } from '../controllers/RegisterSalon/mobile-salon-login';
 import { AddSalonImage } from '../controllers/RegisterSalon/mobile-add-image';
 import {GenerateOTP} from '../controllers/RegisterSalon/generate-Otp';
 import {EmailVerified} from '../controllers/RegisterSalon/email-verified';
+import {ChangePassword} from '../controllers/RegisterSalon/ChangePassword';
+import{ForgotPasswordGenerateOTP} from '../controllers/RegisterSalon/ForgotPasswordGenerateOtp';
 //import { authenticateAdminToken } from '../middlewares/authMiddleware';
 
 export function configureRegisterRoutes(router: Router): void {
@@ -12,4 +14,6 @@ export function configureRegisterRoutes(router: Router): void {
   router.put('/update-salon-image', AddSalonImage);
   router.put('/generate-salon-otp',GenerateOTP);
   router.put('/verified-salon-email',EmailVerified);
+  router.put('/change-salon-password',ChangePassword);
+  router.put('/generate-salon-forgotPassword-otp',ForgotPasswordGenerateOTP);
 }
