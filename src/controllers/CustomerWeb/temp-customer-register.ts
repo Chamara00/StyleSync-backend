90ce22e9fb1e0357d48676fa65e6143b1f8f6f57
Anuradha00Herath <herathanuradha03@gmail.com>
2024-07-11 22:37:05 +0530

    send link to email

--- src/controllers/CustomerWeb/temp-customer-register.ts
+++ src/controllers/CustomerWeb/temp-customer-register.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 import crypto from 'crypto';
 import bcrypt from 'bcrypt';
-import nodemailer from 'nodemailer';
+
 
 const prisma = new PrismaClient();
 
@@ -11,23 +11,8 @@ export async function TempCustomerCreate(req: Request, res: Response){
     const generateTempPassword = () => {
         return Math.random().toString(36).slice(-8); 
       };
-      const transporter = nodemailer.createTransport({
-        service: 'gmail', // You can use any email service
-        auth: {
-          user: 'stylesync26@gmail.com',
-          pass: 'kgjm detu kfpo opsq',
-        },
-      });
-      async function sendOTPEmail(email: string, link: string) {
-        const mailOptions = {
-          from: 'stylesync26@gmail.com',
-          to: email,
-          subject: 'Your OTP Code',
-          text: `Your OTP code is ${link}`,
-        };
       
-        return transporter.sendMail(mailOptions);
-      }
+      
     try{
         if(!email || !contactNo || !userName || !date){
             return res.status(400).json({message: 'Please fill all the fields'});
@@ -49,7 +34,6 @@ export async function TempCustomerCreate(req: Request, res: Response){
             const userId = tempUser.id;
             const token = crypto.randomBytes(16).toString('hex');  // Generate a unique token
             const link = `https://stylesync-backend-test.onrender.com/temporary-login?token=${token}&userId=${userId}`;
-            sendOTPEmail(email,link);
             
             
             // Store the token and associated userId in the database
